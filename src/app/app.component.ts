1b4033cbb49881afc911955c26a6a8004510892e
Diego Almeida <diegoalmda@gmail.com>
2023-12-11 20:00:04 -0300

    Content created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SmallCardComponent } from './components/small-card/small-card.component
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent, HomeComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://www.komar.de/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/8/-/8-4032_2_1.jpg",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme do homem de ferro.",
+  },
+  {
+    "id": "2",
+    "photo": "https://cinebuzz.uol.com.br/media/uploads/as_marvels_sera_o_filme_de_menor_duracao_do_universo_cinematografico_da_marvel.jpg",
+    "title": "Nova série anunciada no disney plus",
+    "description": "bla bla bla bla bla bla bla bla bla bla bla",
+  },
+  {
+    "id": "3",
+    "photo": "https://www.boletimnerd.com.br/wp-content/uploads/2023/05/1543418640-marvel.jpg",
+    "title": "Novo filme da capitã marvel lançado em breve",
+    "description": "bla bla bla bla bla bla bla bla bla bla bla",
+  },
+  {
+    "id": "4",
+    "photo": "https://saopauloparacriancas.com.br/wp-content/uploads/2023/03/SP-criancas_marvel-avenger-station_cred-NEON-PAQUIN_33-scaled.jpeg",
+    "title": "Novo filme do homem de ferro anunciado",
+    "description": "bla bla bla bla bla bla bla bla bla bla bla",
+  },
+]
